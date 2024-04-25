197d24d176d123338d53909fd0ad36f00f55ae58
Benzel <benawhiting@gmail.com>
2024-04-25 16:44:22 -0700

    Configure and implement CORS

--- BookStoreAppAPI/obj/Debug/net8.0/BookStoreAppAPI.AssemblyInfo.cs
+++ BookStoreAppAPI/obj/Debug/net8.0/BookStoreAppAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStoreAppAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fa63d48ac11a45638342b2ea9af6f836b404b81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ebb885060a5015748fd7dc7580768f570f36d8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStoreAppAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStoreAppAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
